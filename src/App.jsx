aaa3cccfe378179b76ea099df3149cb01bb490b7
WesleySid <160278189+WesleySid@users.noreply.github.com>
2024-03-26 09:35:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Header from "./assets/Header";
+import "./App.css";
+import Main from "./assets/Main";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
